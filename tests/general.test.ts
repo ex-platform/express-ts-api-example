75e58d020ad52a17afc77e862e7126b24a14388d
aaron-so <zaiyou12@gmail.com>
2021-10-25 10:12:47 +0900

    ✅ Fix general test

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -5,6 +5,6 @@ describe("Test the root path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('View')
+    expect(response.text).toContain('Hello World!')
   });
 });
